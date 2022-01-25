7b5e072d33ce0b694237557bcd1ba086144d81b4
amargielewski <aleksander.margielewski4@gmail.com>
2022-01-25 16:52:44 +0100

    feat: Add React-Rating

--- src/components/HotelListBox/HotelListBox.tsx
+++ src/components/HotelListBox/HotelListBox.tsx
@@ -6,6 +6,7 @@ import {
   StyledHotelNameText
 } from './HotelListBox.styled';
 import { HotelListImage } from '../HotelListImage/HotelListImage';
+import Rating from 'react-rating';
 
 export const HotelListBox = () => {
   return (
@@ -17,6 +18,7 @@ export const HotelListBox = () => {
           <StyledInfoAddressText>Addres 1</StyledInfoAddressText>
           <StyledInfoAddressText>Addres 2</StyledInfoAddressText>
         </StyledHotelInfoContainer>
+        <Rating />
       </StyledHotelContainer>
     </StyledWrapper>
   );
