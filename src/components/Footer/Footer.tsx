9c0eb495a6fca286c26462ef923b12d8f2b2d5e9
amargielewski <aleksander.margielewski4@gmail.com>
2022-01-30 19:56:05 +0100

    feat:Add footer content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,21 @@
-import { FooterLogo, FooterWrapper } from './Footer.styled';
+import { TextSmall } from '../Typography/Typography.styled';
+import {
+  StyledWrapper,
+  StyledTextContainer,
+  StyledLink
+} from './Footer.styled';
 
 type FooterProps = {};
 
 export const Footer = (props: FooterProps) => {
-  return <FooterWrapper>{/* <FooterLogo>Footer</FooterLogo> */}</FooterWrapper>;
+  return (
+    <StyledWrapper>
+      <StyledTextContainer>
+        <StyledLink target="_blank" href="https://github.com/amargielewski">
+          https://github.com/amargielewski
+        </StyledLink>
+        <TextSmall>Aleksander Margielewski</TextSmall>
+      </StyledTextContainer>
+    </StyledWrapper>
+  );
 };
